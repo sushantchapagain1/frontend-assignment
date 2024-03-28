4eeefc33e29fe97eb25921dd1b96ddcb7fd29ab7
sushant chapagain <sreddevil12@gmail.com>
2024-03-28 14:03:04 +0545

    feat(plant-ui): #1 completed styling of url,sidebar and added tw package

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+import SideNav from '@/components/Sidenav';
+import UrlBar from '@/components/UrlBar';
 import BackgroundImageWrapper from '@/components/BackgroundImageWrapper';
 
 function page() {
   return (
     <BackgroundImageWrapper source="/static/bg-images/bg-1.png">
-      <div className="container mx-auto p-3">app</div>
+      <div className="justify-enter mx-auto flex h-full max-w-[1200px] flex-col items-center justify-between p-6 md:p-20">
+        <UrlBar />
+        <div className="flex w-full flex-col items-center gap-6 md:mt-10 md:flex-row">
+          <SideNav />
+          <main className="h-[450px] w-full self-start justify-self-start rounded-2xl bg-white/10 p-3 backdrop-blur-md">
+            slider
+          </main>
+        </div>
+      </div>
     </BackgroundImageWrapper>
   );
 }
--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -1,14 +1,35 @@
+'use client';
+import Link from 'next/link';
 import { CartIcon, HomeIcon, LockIcon, PlantBucketIcon } from './icons';
+import { usePathname } from 'next/navigation';
+import { cn } from '@/lib/utils';
 
 const links = [
-  { href: '/', icon: HomeIcon },
-  { href: '/plants', icon: PlantBucketIcon },
-  { href: '/cart', icon: CartIcon },
-  { href: '/list', icon: LockIcon },
+  { href: '/', icon: <HomeIcon /> },
+  { href: '/plants', icon: <PlantBucketIcon /> },
+  { href: '/cart', icon: <CartIcon /> },
+  { href: '/list', icon: <LockIcon /> },
 ] as const;
 
 function SideNav() {
-  return <div>SideNav</div>;
+  const pathName = usePathname();
+  return (
+    <nav className="rounded-full border bg-white/60 backdrop-blur-xl">
+      <ul className="flex items-center justify-between gap-4 md:flex-col md:p-2">
+        {links.map((link) => {
+          const isActive = pathName === link.href;
+          return (
+            <li
+              key={link.href}
+              className={cn('p-2', isActive && 'rounded-full bg-white/35')}
+            >
+              <Link href={link.href}>{link.icon}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
 }
 
 export default SideNav;
